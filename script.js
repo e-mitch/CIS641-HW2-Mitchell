ee3c254af930b131d63c7e9e8acca697b302b2cd
boromirthebold@gmail.com <boromirthebold@gmail.com>
2021-09-25 15:51:04 -0400

    restructured to add left header bar

--- script.js
+++ script.js
@@ -1,64 +1,19 @@
-var leftElements = document.getElementsByClassName("left");
-var rightElements = document.getElementsByClassName("right");
-
-function setupPage() {
-  for(var i=0; i < leftElements.length; i++){
-    leftElements[i].style.display="none";
-    rightElements[i].style.display="none";
-  }
-  document.getElementById("interestsText").style.display="none";
-  document.getElementById("technicalText").style.display="none";
-  document.getElementById("projectText").style.display="none";
-}
-
-function toggleSchool(){
-  if(leftElements[0].style.display == "none"){
-    document.getElementsByClassName("left")[0].style.display = "inline";
-    document.getElementsByClassName("right")[0].style.display = "inline";
-    document.getElementsByClassName("left")[1].style.display = "inline";
-    document.getElementsByClassName("right")[1].style.display = "inline";
-  } else {
-    document.getElementsByClassName("left")[0].style.display = "none";
-    document.getElementsByClassName("right")[0].style.display = "none";
-    document.getElementsByClassName("left")[1].style.display = "none";
-    document.getElementsByClassName("right")[1].style.display = "none";
-  }
-}
-
-function toggleWork(){
-  if(leftElements[2].style.display == "none"){
-    document.getElementsByClassName("left")[2].style.display = "inline";
-    document.getElementsByClassName("right")[2].style.display = "inline";
-    document.getElementsByClassName("left")[3].style.display = "inline";
-    document.getElementsByClassName("right")[3].style.display = "inline";
-  } else {
-    document.getElementsByClassName("left")[2].style.display = "none";
-    document.getElementsByClassName("right")[2].style.display = "none";
-    document.getElementsByClassName("left")[3].style.display = "none";
-    document.getElementsByClassName("right")[3].style.display = "none";
-  }
-}
-
-function toggleInterests(){
-  if(document.getElementById("interestsText").style.display == "none"){
-    document.getElementById("interestsText").style.display = "block";
-  } else {
-    document.getElementById("interestsText").style.display = "none";
-  }
-}
-
-function toggleTechnical(){
-  if(document.getElementById("technicalText").style.display == "none"){
-    document.getElementById("technicalText").style.display = "block";
-  } else {
-    document.getElementById("technicalText").style.display = "none";
-  }
-}
-
-function toggleProject(){
-  if(document.getElementById("projectText").style.display == "none"){
-    document.getElementById("projectText").style.display = "block";
-  } else {
-    document.getElementById("projectText").style.display = "none";
+function toggleVisibility(clickedElement){
+  var sectionHeaders = document.getElementById("sectionHeaders").getElementsByTagName("h3");
+  var textDivs = document.getElementsByClassName("textDiv");
+  for(var i = 0; i < textDivs.length; i++){
+    var label = sectionHeaders[i].id;
+    if(textDivs[i].id != clickedElement){
+      textDivs[i].style.display = "none";
+      sectionHeaders[i].style.border="none";
+    } else {
+      if(textDivs[i].style.display == "inline"){
+        textDivs[i].style.display = "none";
+        sectionHeaders[i].style.border = "none";
+      } else {
+        textDivs[i].style.display = "inline";
+        sectionHeaders[i].style.border = "1px solid #D9E76C";
+      }
+    }
   }
 }
